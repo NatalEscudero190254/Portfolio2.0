34d1c1a50d7f3eea14d75f8b0f97c2b47bcdfbe8
NatalEscudero190254 <102334080+NatalEscudero190254@users.noreply.github.com>
2024-03-17 21:12:19 -0300

    corrijo errores de ortografia

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,23 +5,23 @@ const AboutMe = ({ img }) => {
     <article className="flex flex-col md:flex-row gap-8 justify-center items-center">
       <div className=" text-pretty [&>p]:mb-4 [&>p>strong]:font-semibold [&>p>strong]:text-blue-600">
         <p>
-          Me llamo Natal, pero mis amigos me dicen Nata. La tecnologia me gusto
+          Me llamo Natal, pero mis amigos me dicen Nata. La tecnología me gustó
           desde siempre,{" "}
           <strong>
-            empeze por el hardware desarmando componentes a temprana edad
+            empecé por el hardware desarmando componentes a temprana edad
           </strong>
           , (siempre me dio curiosidad) y actualmente me encuentro en React
-          creandolos.
+          creándolos.
         </p>
         <p>
           <strong>Junto con el deporte</strong>, sentarme a programar en mi
-          habitacion es algo que me libera de estres, aunque a veces sea
+          habitación es algo que me libera de estrés, aunque a veces sea
           frustrante.{" "}
         </p>
         <p>
-          Disfruto creando aplicaciones web y aprendiendo todos los dias un
-          poquito mas de este{" "}
-          <strong>maravilloso mundo que esta en constante cambio</strong>.
+          Disfruto creando aplicaciones web y aprendiendo todos los días un
+          poquito más de este{" "}
+          <strong>mundo que está en constante cambio</strong>.
         </p>
       </div>
       <img
