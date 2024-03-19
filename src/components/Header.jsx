cd5e5790487986e27344cece682a8986d5e26f41
NatalEscudero190254 <102334080+NatalEscudero190254@users.noreply.github.com>
2024-03-18 21:18:22 -0300

    fix header color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="flex justify-center items-center mx-auto py-5 sticky top-0 w-full z-10 text-white">
-      <nav className="border  border-black rounded-md px-5 py-3 bg-neutral-200 bg-black/50 backdrop-blur-xl  flex flex-row gap-10 ">
+      <nav className="border  border-black rounded-md px-5 py-3  bg-black/50 backdrop-blur-xl  flex flex-row gap-10 ">
         <a
           className="hover:drop-shadow-custom px-3 rounded-md transition"
           href="#experiencia"
